8d5c7f5a354aba3af18dd9d7a5c7d90e8a7d7673
Sven Boeckelmann <sboeckelmann@benelog.com>
2023-02-10 17:27:03 +0100

    add support for schemaVersion string to EPCISVersion improve documatation

--- src/main/java/io/openepcis/constants/EPCISVersion.java
+++ src/main/java/io/openepcis/constants/EPCISVersion.java
@@ -3,28 +3,49 @@ package io.openepcis.constants;
 import java.util.Optional;
 
 public enum EPCISVersion {
-  VERSION_2_0_0("2.0.0"),
-  VERSION_1_2_0("1.2.0");
+  // shall be in ascending order for min/max comparison
+  VERSION_1_2_0("1.2.0", "1.2"),
+  VERSION_2_0_0("2.0.0", "2.0");
 
   private final String version;
 
+  private final String schemaVersion;
+
+  /**
+   * Method to return fully-qualified EPCIS Version used in http headers: GS1-EPCIS-Version,
+   * GS1-EPCIS-Min, GS1-EPCIS-Max
+   *
+   * @return fully qualified version ex: 1.2.0, 2.0.0
+   */
   public String getVersion() {
     return this.version;
   }
 
-  EPCISVersion(final String version) {
+  /**
+   * Method to return schemaVersion string commonly used in EPCIS documents example:
+   * "schemaVersion": "2.0"
+   *
+   * @return compatible schemaVersion ex: 1.2, 2.0
+   */
+  public String getSchemaVersion() {
+    return this.schemaVersion;
+  }
+
+  EPCISVersion(final String version, final String schemaVersion) {
     this.version = version;
+    this.schemaVersion = schemaVersion;
   }
 
   /**
    * Method to return respective EPCIS version based on provided string input version.
    *
-   * @param version input version provided by user ex: 2.0, 1.2, 2.0.0.
-   * @return version returned based on the provided string version ex: 2.0.0 or 1.2.0.
+   * @param s input version provided by user ex: 2.0, 1.2, 2.0.0, 1.2.0.
+   * @return EPCISVersion returned based on the provided version or schemaVersion string ex:
+   *     VERSION_1_2_0 or VERSION_2_2_0.
    */
-  public static Optional<EPCISVersion> fromString(final String version) {
+  public static Optional<EPCISVersion> fromString(final String s) {
     for (EPCISVersion v : values()) {
-      if (v.version.equals(version)) {
+      if (v.version.equals(s) || v.schemaVersion.equals(s)) {
         return Optional.of(v);
       }
     }
